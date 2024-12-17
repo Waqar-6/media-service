8f57dd460a6a1d5fc7fc6a0a0e3a3a4d47c1cbfe
Waqar <waqar-1@hotmail.co.uk>
2024-12-17 23:38:24 +0000

    chore: created empty project

---
+++ src/test/java/com/w_farooq_group/mediaservice/MediaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
